1ecd3247270bc5a7381afee7d4cdd910a85ac606
augusto-fstc <126532965+augusto-fstc@users.noreply.github.com>
2023-10-25 13:28:41 -0300

    PRIMEIRO COMMIT

---
+++ src/routes/products.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require('../controllers/productsController');
+
+router.post('/products', productsController.createProduct);
+router.get('/products', productsController.getProducts);
+
+router.get('/', (req, res) => {
+    res.send('Hello Word!')
+})
+
+module.exports = router;
